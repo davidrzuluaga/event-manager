71c9ded82320d0dab786377948cb427a016d1330
David Ricardo Zuluaga <david@davidrzuluaga.com>
2020-07-20 15:07:03 -0500

    Updates

---
+++ precache-manifest.e8fda26bbf55c583234e9b7046892918.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "519cbb9f5ed84373049a7b82edbefc48",
+    "url": "/event-manager/index.html"
+  },
+  {
+    "revision": "524401694d38d8faa0ce",
+    "url": "/event-manager/static/css/main.ad276fdf.chunk.css"
+  },
+  {
+    "revision": "6caeab70b8f0bf580565",
+    "url": "/event-manager/static/js/2.8c967227.chunk.js"
+  },
+  {
+    "revision": "b4cabae32341920cfb4131dbdebda2c2",
+    "url": "/event-manager/static/js/2.8c967227.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "524401694d38d8faa0ce",
+    "url": "/event-manager/static/js/main.5f9cbbf7.chunk.js"
+  },
+  {
+    "revision": "328c7d2eca3d7b121173",
+    "url": "/event-manager/static/js/runtime-main.eec15f87.js"
+  }
+]);
